aad449a01fa1b869b5b604c355564cec7452d638
Hanafi Yaming <142217119+jhin3tytwoo@users.noreply.github.com>
2024-10-25 16:48:15 +0700

    update report page

--- src/app/api/job/job-report/route.js
+++ src/app/api/job/job-report/route.js
@@ -12,33 +12,52 @@ export const dynamic = "force-dynamic";
  * @description Find All Prompts and return
  */
 
-
 export const GET = async (req, res) => {
   try {
     await connectToDb();
 
-   
     const jobCounts = await User.aggregate([
       {
         $lookup: {
-          from: "jobs", // The name of the Job collection
+          from: "jobs",
           localField: "_id",
           foreignField: "ACTIVATE_USER",
-          as: "jobs"
-        }
+          as: "jobs",
+        },
+      },
+      {
+        $lookup: {
+          from: "workgroups", // เชื่อมต่อกับกลุ่มงาน
+          localField: "_id",
+          foreignField: "USER_LIST", // เชื่อมต่อกับ USER_LIST ใน workgroup
+          as: "workgroup",
+        },
+      },
+      {
+        $unwind: {
+          path: "$workgroup",
+          preserveNullAndEmptyArrays: true,
+        },
       },
       {
         $project: {
-          userName: "$EMP_NAME", // Assuming the User schema has an 'EMP_NAME' field
-          jobCount: { $size: "$jobs" }
-        }
+          userName: "$EMP_NAME",
+          jobCount: { $size: "$jobs" },
+          team: "$TEAM",
+          JOB_NAME: "$jobs.JOB_NAME",
+          LINE_NAME: "$jobs.LINE_NAME",
+          createdAt: "$jobs.createdAt",
+          workgroupName: {
+            $ifNull: ["$workgroup.WORKGROUP_NAME", "ไม่มีกลุ่มงาน"],
+          }, // แสดงกลุ่มงาน
+        },
       },
       {
-        $sort: { jobCount: -1 } // Sort by jobCount in descending order
-      }
+        $sort: { jobCount: -1 },
+      },
     ]);
 
-    console.log("Job counts:", jobCounts);
+    console.log("Job counts with additional fields:", jobCounts);
 
     return NextResponse.json(jobCounts);
   } catch (error) {
--- src/app/pages/report/BarChart.js
+++ src/app/pages/report/BarChart.js
@@ -1,7 +1,6 @@
-// components/BarChart.js
 "use client";
-import React, { useState, useEffect } from "react";
-import { Bar } from "react-chartjs-2";
+import React, { useState, useRef } from "react";
+import { Bar, Line, Pie } from "react-chartjs-2";
 import {
   Chart as ChartJS,
   BarElement,
@@ -11,8 +10,13 @@ import {
   Title,
   Tooltip,
   Legend,
-} from "chart.js"; // เพิ่ม LogarithmicScale
+  ArcElement,
+} from "chart.js";
 import useFetchReport from "@/lib/hooks/useFetchReport";
+import useFetchUsers from "@/lib/hooks/useFetchUser";
+import * as FileSaver from "file-saver";
+import * as XLSX from "xlsx";
+import { FaFileCsv, FaImage } from "react-icons/fa";
 
 ChartJS.register(
   BarElement,
@@ -21,75 +25,309 @@ ChartJS.register(
   LogarithmicScale,
   Title,
   Tooltip,
-  Legend
-); // ลงทะเบียน LogarithmicScale
+  Legend,
+  ArcElement
+);
 
 const BarChart = () => {
   const [refresh, setRefresh] = useState(false);
-  const { report, isLoading, error } = useFetchReport(refresh);
+  const [chartType, setChartType] = useState("bar");
+  const [topN, setTopN] = useState("all");
+  const [selectedWorkgroup, setSelectedWorkgroup] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  const { report } = useFetchReport(refresh);
+  const { user, isLoading: usersloading } = useFetchUsers(refresh);
+  const chartRef = useRef(null);
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const filterReportByWorkgroup = (data, workgroup) => {
+    if (workgroup === "") return data;
+    return data.filter((item) => item.workgroupName === workgroup);
+  };
 
-  // กรองผู้ใช้งานที่มีจำนวน Checklist ต่ำกว่า 5 ออก
-  const filteredReport = report.filter((item) => item.jobCount > 5);
-  // เพิ่มกลุ่ม "Others" สำหรับผู้ใช้งานที่มีจำนวน Checklist ต่ำกว่า 5
-  const otherUsers = report.filter((item) => item.jobCount <= 5);
-  const reducedReport = [
-    ...filteredReport,
-    {
-      userName: "Others",
-      jobCount: otherUsers.reduce((acc, cur) => acc + cur.jobCount, 0),
-    },
+  const filterReportByDateRange = (data, startDate, endDate) => {
+    if (!startDate && !endDate) return data;
+    return data.filter((item) => {
+      const itemDate = new Date(item.createdAt[0]);
+      const isAfterStartDate = startDate
+        ? itemDate >= new Date(startDate)
+        : true;
+      const isBeforeEndDate = endDate ? itemDate <= new Date(endDate) : true;
+      return isAfterStartDate && isBeforeEndDate;
+    });
+  };
+
+  const getTopNReport = (data, topN) => {
+    const sortedData = data.sort((a, b) => b.jobCount - a.jobCount);
+    let mainData = [];
+    let otherData = [];
+
+    if (topN === "top5") {
+      mainData = sortedData.slice(0, 5);
+    } else if (topN === "top10") {
+      mainData = sortedData.slice(0, 10);
+    } else {
+      return sortedData;
+    }
+
+    otherData = sortedData.slice(mainData.length);
+    return [
+      ...mainData,
+      {
+        userName: "Others",
+        jobCount: otherData.reduce((acc, cur) => acc + cur.jobCount, 0),
+      },
+    ];
+  };
+
+  // ฟิลเตอร์ข้อมูลตาม Workgroup และ Date
+  const filteredReport = filterReportByDateRange(
+    filterReportByWorkgroup(report, selectedWorkgroup),
+    startDate,
+    endDate
+  );
+
+  const finalReport = getTopNReport(filteredReport, topN);
+
+  const workgroupOptions = [
+    ...new Set(report.map((item) => item.workgroupName)),
   ];
 
   const data = {
-    labels: reducedReport.map((item) => item.userName),
+    labels: finalReport.map((item) => item.userName),
     datasets: [
       {
         label: "Number of Checklists Activated",
-        backgroundColor: reducedReport.map(
+        backgroundColor: finalReport.map(
           () => "#" + Math.floor(Math.random() * 16777215).toString(16)
         ),
-        data: reducedReport.map((item) => item.jobCount),
+        data: finalReport.map((item) => item.jobCount),
       },
     ],
   };
 
+  const exportToCSV = () => {
+    const formattedData = finalReport.map((item) => ({
+      userName: item.userName,
+      jobCount: item.jobCount,
+      JOB_NAME: Array.isArray(item.JOB_NAME) ? item.JOB_NAME.join(", ") : "",
+      LINE_NAME: Array.isArray(item.LINE_NAME) ? item.LINE_NAME.join(", ") : "",
+      createdAt: Array.isArray(item.createdAt)
+        ? item.createdAt
+            .map((date) => new Date(date).toLocaleDateString())
+            .join(", ")
+        : "",
+    }));
+
+    const ws = XLSX.utils.json_to_sheet(formattedData);
+    const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+    const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+    const data = new Blob([excelBuffer], { type: "application/octet-stream" });
+
+    // สร้างชื่อไฟล์ตามกลุ่มงานและ topN ที่เลือก
+    const workgroupName = selectedWorkgroup || "All_Workgroups"; // หากไม่เลือกให้ใช้ชื่อ All_Workgroups
+    const fileName = `${workgroupName}_top_${topN}.xlsx`; // สร้างชื่อไฟล์
+
+    FileSaver.saveAs(data, fileName); // ใช้ชื่อไฟล์ที่สร้างขึ้น
+  };
+
+  const saveAsPNG = () => {
+    const chart = chartRef.current;
+    if (chart) {
+      const url = chart.toBase64Image();
+      const workgroupName = selectedWorkgroup || "All_Workgroups"; // หากไม่เลือกให้ใช้ชื่อ All_Workgroups
+      const fileName = `${workgroupName}_top_${topN}.png`; // สร้างชื่อไฟล์
+
+      const link = document.createElement("a");
+      link.href = url;
+      link.download = fileName; // ใช้ชื่อไฟล์ที่สร้างขึ้น
+      link.click();
+    }
+  };
+
+  const handleExport = (option) => {
+    if (option === "csv") {
+      exportToCSV();
+    } else if (option === "png") {
+      saveAsPNG();
+    }
+  };
+
   const options = {
     responsive: true,
+    maintainAspectRatio: false,
     plugins: {
-      legend: {
-        position: "top",
-      },
+      legend: { position: "top" },
       title: {
         display: true,
         text: "Top of Checklists Activated by Each Employee Name",
       },
     },
     scales: {
-      x: {
-        title: {
-          display: true,
-          text: "Employee Name",
-        },
-      },
+      x: { title: { display: true, text: "Employee Name" } },
       y: {
-        type: "logarithmic", // ใช้สเกลแบบ Logarithmic เพื่อลดความไม่สมดุลของข้อมูล
-        title: {
-          display: true,
-          text: "Number of Checklists",
-        },
+        type: "logarithmic",
+        title: { display: true, text: "Number of Checklists" },
       },
     },
   };
 
-  return <Bar data={data} options={options} />;
+  const today = new Date();
+  const [selectedMonths, setSelectedMonths] = useState([]);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
+  const handleCheckboxChange = (monthIndex) => {
+    const newSelectedMonths = selectedMonths.includes(monthIndex)
+      ? selectedMonths.filter((month) => month !== monthIndex)
+      : [...selectedMonths, monthIndex];
+
+    setSelectedMonths(newSelectedMonths);
+
+    if (newSelectedMonths.length > 0) {
+      const startMonth = Math.min(...newSelectedMonths);
+      const endMonth = Math.max(...newSelectedMonths);
+      // ตั้งค่า start date และ end date ตามเดือนที่เลือก
+      setStartDate(
+        new Date(today.getFullYear(), startMonth, 1).toISOString().slice(0, 10)
+      );
+      setEndDate(
+        new Date(today.getFullYear(), endMonth + 1, 0)
+          .toISOString()
+          .slice(0, 10)
+      );
+    } else {
+      setStartDate("");
+      setEndDate("");
+    }
+  };
+
+  return (
+    <div>
+      <div className="flex flex-wrap justify-between mb-4">
+        <div className="mb-4 md:w-1/4">
+          <p className="mb-2 font-semibold text-lg">Select Months:</p>
+          <button
+            onClick={toggleDropdown}
+            className="border border-gray-300 rounded-md p-2 w-full text-left"
+          >
+            {selectedMonths.length > 0
+              ? `Selected ${selectedMonths.length} months`
+              : "Select Months"}
+          </button>
+          {dropdownOpen && (
+            <div className="absolute bg-white border border-gray-300 rounded-md mt-1 max-h-60 overflow-y-auto z-10">
+              {Array.from({ length: 12 }, (_, index) => {
+                const monthName = new Date(
+                  today.getFullYear(),
+                  index
+                ).toLocaleString("default", { month: "long" });
+                return (
+                  <div
+                    key={index}
+                    className="flex items-center mb-1 p-2 hover:bg-gray-100"
+                  >
+                    <input
+                      type="checkbox"
+                      id={`month_${index}`}
+                      value={index}
+                      checked={selectedMonths.includes(index)}
+                      onChange={() => handleCheckboxChange(index)}
+                      className="mr-2"
+                    />
+                    <label
+                      htmlFor={`month_${index}`}
+                      className="cursor-pointer"
+                    >
+                      {monthName}
+                    </label>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </div>
+        <div className="mb-4 w-full md:w-1/4">
+          <input
+            type="date"
+            value={startDate}
+            onChange={(e) => setStartDate(e.target.value)}
+            className="border border-gray-300 rounded-md p-2 mr-2 w-full"
+          />
+          <input
+            type="date"
+            value={endDate}
+            onChange={(e) => setEndDate(e.target.value)}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          />
+        </div>
+        <div className="mb-4 w-full md:w-1/4">
+          <select
+            value={selectedWorkgroup}
+            onChange={(e) => setSelectedWorkgroup(e.target.value)}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          >
+            <option value="">All Workgroups</option>
+            {workgroupOptions.map((workgroup) => (
+              <option key={workgroup} value={workgroup}>
+                {workgroup}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className="mb-4 w-full md:w-1/4">
+          <select
+            value={chartType}
+            onChange={(e) => setChartType(e.target.value)}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          >
+            <option value="bar">Bar</option>
+            <option value="line">Line</option>
+            <option value="pie">Pie</option>
+          </select>
+        </div>
+        <div className="mb-4 w-full md:w-1/4">
+          <select
+            value={topN}
+            onChange={(e) => setTopN(e.target.value)}
+            className="border border-gray-300 rounded-md p-2 w-full"
+          >
+            <option value="all">All</option>
+            <option value="top5">Top 5</option>
+            <option value="top10">Top 10</option>
+          </select>
+        </div>
+        <div className="flex w-full md:w-1/4 mb-4">
+          <button
+            onClick={() => handleExport("csv")}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md mr-2 transition duration-300"
+          >
+            Export CSV <FaFileCsv />
+          </button>
+          <button
+            onClick={() => handleExport("png")}
+            className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md transition duration-300"
+          >
+            Save as PNG <FaImage />
+          </button>
+        </div>
+      </div>
+
+      <div style={{ height: "400px", width: "100%" }}>
+        {chartType === "bar" && (
+          <Bar data={data} options={options} ref={chartRef} />
+        )}
+        {chartType === "line" && (
+          <Line data={data} options={options} ref={chartRef} />
+        )}
+        {chartType === "pie" && (
+          <Pie data={data} options={options} ref={chartRef} />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default BarChart;
