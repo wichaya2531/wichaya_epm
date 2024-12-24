5c608147c05042cbcbe9c290832a87637e5edd95
Hanafi Yaming <142217119+jhin3tytwoo@users.noreply.github.com>
2024-12-24 15:58:09 +0700

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,15 +5,17 @@ import Image from "next/image";
 const Card = ({ title = "", detail = "", link = "#", logo_path }) => {
   return (
     <Link
-      className="size-52 bg-white drop-shadow-lg rounded-xl hover:shadow-2xl cursor-pointer flex flex-col gap-3 p-6 justify-center items-start transition-transform duration-300 transform hover:scale-105"
+      className="size-52 bg-white drop-shadow-lg rounded-xl hover:shadow-2xl cursor-pointer flex flex-col gap-3 p-6 justify-center transition-transform duration-300 transform hover:scale-105"
       href={link[0]}
     >
-      <Image
-        src={logo_path || "/assets/card-logo/NotFound.png"}
-        width={50}
-        height={50}
-        alt="card-logo"
-      />
+      <div className="flex justify-center items-center">
+        <Image
+          src={logo_path || "/assets/card-logo/NotFound.png"}
+          width={50}
+          height={50}
+          alt="card-logo"
+        />
+      </div>
       <div className="flex flex-col gap-3">
         <h1 className="text-2xl font-bold text-primary">{title}</h1>
         <p className="text-sm text-secondary max-h-48">{detail}</p>
