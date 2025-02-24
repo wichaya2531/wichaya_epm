b115e00026a0aef2421cc2ad37545e1e380c3267
wichaya2531 <82375043+wichaya2531@users.noreply.github.com>
2025-02-24 09:07:42 +0700

    commit-manage-machine-id/name

---
+++ src/app/pages/manage-machine-id/page.js
@@ -0,0 +1,15 @@
+"use client";
+import Layout from "@/components/Layout.js";
+const Page = () => {
+ 
+
+  return (
+    <Layout className="container flex flex-col left-0 right-0 mx-auto justify-start font-sans mt-2 px-6 gap-10">
+            <div>
+                 Hello World
+            </div>
+    </Layout>
+  );
+};
+
+export default Page;
